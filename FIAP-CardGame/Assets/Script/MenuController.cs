6f326ff8ecbf023c05030bc77cd59dd101d66fd7
willian matheus <willian_matheus14@hotmail.com>
2019-05-28 22:50:23 -0300

    Modelo 0.0.2

---
+++ FIAP-CardGame/Assets/Script/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject generalmenu;
+    public GameObject optionsmenu;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        ActiveMenu(generalmenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HideMenu()
+    {
+        generalmenu.SetActive(false);
+        optionsmenu.SetActive(false);
+    }
+
+    public void ActiveMenu (GameObject menu)
+    {
+        HideMenu();
+        menu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        ApplicationController.ExitGame();
+
+    }
+
+}

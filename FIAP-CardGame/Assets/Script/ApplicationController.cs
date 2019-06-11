fd3f7d79fe8f7f0480ca9b145f591847d52b33f2
willian matheus <willian_matheus14@hotmail.com>
2019-06-10 22:35:24 -0300

    import 0.0.3

--- FIAP-CardGame/Assets/Script/ApplicationController.cs
+++ FIAP-CardGame/Assets/Script/ApplicationController.cs
@@ -20,4 +20,70 @@ public class ApplicationController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    //sound configuration
+
+    //SFX
+    public static void EnableSoundSFX()
+    {
+        PlayerPrefs.SetInt("SFXSound", 1);
+    }
+
+    public static void DisableSoundSFX()
+    {
+        PlayerPrefs.SetInt("SFXSound", 0);
+    }
+
+    public static bool IsMuttedSoundSFX()
+    {
+        if(PlayerPrefs.GetInt("SFXSound") == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void SetVolumeSFX(float volume)
+    {
+        PlayerPrefs.SetFloat("SFXSoundVolume", volume);
+    }
+
+    public static float GetVolumeSFX()
+    {
+        return PlayerPrefs.GetFloat("SFXSoundVolume");
+    }
+
+    //Music
+    public static void EnableSoundMusic()
+    {
+        PlayerPrefs.SetInt("MusicSound", 1);
+    }
+
+    public static void DisableSoundMusic()
+    {
+        PlayerPrefs.SetInt("MusicSound", 0);
+    }
+
+    public static bool IsMuttedSoundMusic()
+    {
+        if (PlayerPrefs.GetInt("MusicSound") == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void SetVolumeMusic(float volume)
+    {
+        PlayerPrefs.SetFloat("MusicSoundVolume", volume);
+    }
+
+    public static float GetVolumeMusic()
+    {
+        return PlayerPrefs.GetFloat("MusicSoundVolume");
+    }
+
+    //sound configuration
 }
---
+++ FIAP-CardGame/Assets/Script/OptionsMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour
+{
+
+    public Toggle toggleSoundSFX;
+    public Toggle toggleSoundMusic;
+    public Slider sliderVolumeSFX;
+    public Slider sliderVolumeMusic;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleSoundSFX.isOn = ApplicationController.IsMuttedSoundSFX();
+        toggleSoundMusic.isOn = ApplicationController.IsMuttedSoundMusic();
+        sliderVolumeSFX.value = ApplicationController.GetVolumeSFX();
+        sliderVolumeMusic.value = ApplicationController.GetVolumeMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
